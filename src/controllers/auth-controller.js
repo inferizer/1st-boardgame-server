b949a5196569c80fad7e997e555033a1a2c2f2cd
inferizer <codecamp.1412@gmail.com>
2023-10-10 11:25:04 +0700

    acccess token

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,4 +1,5 @@
 const { registerSchema } = require("../validators/auth-validator");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const prisma = require("../models/prisma");
 
@@ -13,7 +14,18 @@ exports.register = async (req, res, next) => {
     const user = await prisma.users.create({
       data: value,
     });
-    res.status(200).json({ message: "register success" });
+    console.log(user);
+    const payload = {
+      email: user.email,
+    };
+    const SECRET = process.env.JWT_SECRET;
+    const EXP = process.env.JWT_EXP;
+    const accessToken = jwt.sign(payload, SECRET || "6546sajkhsajkdh", {
+      expiresIn: EXP,
+    });
+
+    delete user.password;
+    res.status(201).json({ accessToken, user });
   } catch (error) {
     return next(error);
   }
